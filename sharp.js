fb0d449965cdb25df35cd8431ba6633e2111f446
SalvaToro <sftpo@protonmail.ch>
2023-05-19 18:15:08 +0200

    Exercise 6 commented

--- sharp.js
+++ sharp.js
@@ -6,16 +6,17 @@ import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__filename);
 
 const exerciseImgs = glob.sync("./src/html-css/ejercicio-6/**/*.jpg");
 
-exerciseImgs.forEach((img) => {
-  const newName = img.replace("jpg", "webp");
-  console.log("Starting to process: ", img);
-  sharp(img)
-    .toFile(newName)
-    .then(() => {
-      // fs.copyFileSync(newName, newName.replace("js/ejercicio-04", "public"));
-      console.log("Done with: ", newName);
-    });
-});
+// exerciseImgs.forEach((img) => {
+//   const newName = img.replace("jpg", "webp");
+//   console.log("Starting to process: ", img);
+//   sharp(img)
+//     .toFile(newName)
+//     .then(() => {
+//       // fs.copyFileSync(newName, newName.replace("js/ejercicio-04", "public"));
+//       console.log("Done with: ", newName);
+//     });
+// });
