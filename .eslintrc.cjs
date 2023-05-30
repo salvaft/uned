3ced5430d966d0c26df9585a8f40d01f17911a22
SalvaToro <sftpo@protonmail.ch>
2023-05-30 19:21:09 +0200

    fix: Added jquery to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jquery: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
