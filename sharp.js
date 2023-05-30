5fcc603509ab322f0f666fa416119e8a0c84a4e0
SalvaToro <sftpo@protonmail.ch>
2023-05-30 19:21:34 +0200

    fix: Transformed pictures for ex6

--- sharp.js
+++ sharp.js
@@ -7,17 +7,18 @@ import { glob } from "glob";
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-const extension = "PNG";
-const exerciseImgs = glob.sync(`./src/html-css/ejercicio-6/**/*.${extension}`);
-const alreadyTransformed = glob.sync(`./src/html-css/ejercicio-6/**/*.webp`);
+const extension = "png";
+const exerciseImgs = glob.sync(`./src/js/ejercicio-6/**/*.${extension}`);
+const alreadyTransformed = glob.sync(`./src/js/ejercicio-6/**/*.webp`);
 
 exerciseImgs.forEach((img) => {
   const newName = img.replace(`${extension}`, "webp");
   if (alreadyTransformed.includes(newName)) return;
   console.log("Starting to process: ", img);
-  sharp(img).toFile(newName);
-  //     .then(() => {
-  //       // fs.copyFileSync(newName, newName.replace("js/ejercicio-04", "public"));
-  //       console.log("Done with: ", newName);
-  //     });
+  sharp(img)
+    .toFile(newName)
+    .then(() => {
+      //       // fs.copyFileSync(newName, newName.replace("js/ejercicio-04", "public"));
+      console.log("Done with: ", newName);
+    });
 });
