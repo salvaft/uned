4e9f48eba06194eef9ba8113d41601a86205e0c1
SalvaToro <sftpo@protonmail.ch>
2023-05-31 17:59:01 +0200

    fix: Disabled url inlining of assets

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,26 @@ import { defineConfig } from "vite";
 import { glob } from "glob";
 import path, { dirname } from "path";
 
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig(
-  {
-
-    plugins: [react()],
-    root: path.join(__dirname, "src"),
-    resolve: {
-      alias: {
-        "@src": path.resolve(__dirname, "./src"),
-        "@lib": path.resolve(__dirname, "./src/lib"),
-      },
+export default defineConfig({
+  plugins: [react()],
+  root: path.join(__dirname, "src"),
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
     },
-    build: {
-      outDir: path.join(__dirname, "dist"),
-      rollupOptions: {
-        input: glob.sync(path.resolve(__dirname, "src/**/*.html")
-        ),
-      },
+  },
+  build: {
+    outDir: path.join(__dirname, "dist"),
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: glob.sync(path.resolve(__dirname, "src/**/*.html")),
     },
-  }
-
-
-);
+  },
+});
