d92adfdae622d1ad4005fc4ab5514e4aca094aad
SalvaToro <sftpo@protonmail.ch>
2023-05-31 14:03:22 +0200

    docs: Styled comments ex js 6-1

--- src/js/ejercicio-6/js/Ejercicio_6-1.js
+++ src/js/ejercicio-6/js/Ejercicio_6-1.js
@@ -1,11 +1,13 @@
 "use strict";
-//$( handler )
-// $( document ).ready( handler )
-// $( "document" ).ready( handler )
-// $( "img" ).ready( handler )
-// $().ready( handler )
-// As of jQuery 3.0, only the first syntax is recommended; the other syntaxes still work but are deprecated. This is because the selection has no bearing on the behavior of the .ready() method, which is inefficient and can lead to incorrect assumptions about the method's behavior. For example, the third syntax works with "document" which selects nothing. The fourth syntax waits for the document to be ready but implies (incorrectly) that it waits for images to become ready.
-// https://api.jquery.com/ready/
+/*
+ * $( handler )
+ * $( document ).ready( handler )
+ * $( "document" ).ready( handler )
+ * $( "img" ).ready( handler )
+ * $().ready( handler )
+ * As of jQuery 3.0, only the first syntax is recommended; the other syntaxes still work but are deprecated. This is because the selection has no bearing on the behavior of the .ready() method, which is inefficient and can lead to incorrect assumptions about the method's behavior. For example, the third syntax works with "document" which selects nothing. The fourth syntax waits for the document to be ready but implies (incorrectly) that it waits for images to become ready.
+ * https://api.jquery.com/ready/
+ */
 $(() => {
   $("#donald").on("click", () => {
     $("#donald").attr("src", "img/Donald_1.webp");
