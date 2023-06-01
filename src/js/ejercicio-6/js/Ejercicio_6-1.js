0926678413401e7f0d714784fb8c9e4bec8833a1
SalvaToro <sftpo@protonmail.ch>
2023-06-01 11:46:18 +0200

    fix: Imported imgs for bundling

--- src/js/ejercicio-6/js/Ejercicio_6-1.js
+++ src/js/ejercicio-6/js/Ejercicio_6-1.js
@@ -8,17 +8,22 @@
  * As of jQuery 3.0, only the first syntax is recommended; the other syntaxes still work but are deprecated. This is because the selection has no bearing on the behavior of the .ready() method, which is inefficient and can lead to incorrect assumptions about the method's behavior. For example, the third syntax works with "document" which selects nothing. The fourth syntax waits for the document to be ready but implies (incorrectly) that it waits for images to become ready.
  * https://api.jquery.com/ready/
  */
+
+import Goofi_1 from "../img/Goofi_1.webp";
+import Goofi_2 from "../img/Goofi_2.webp";
+import Donald_1 from "../img/Donald_1.webp";
+import Donald_2 from "../img/Donald_2.webp";
 $(() => {
   $("#donald").on("click", () => {
-    $("#donald").attr("src", "img/Donald_1.webp");
+    $("#donald").attr("src", Donald_1);
   });
   $("#donald").on("dblclick", () => {
-    $("#donald").attr("src", "img/Donald_2.webp");
+    $("#donald").attr("src", Donald_2);
   });
   $("#donald").on("mouseover", () => {
-    $("#donald").attr("src", "img/Goofi_1.webp");
+    $("#donald").attr("src", Goofi_1);
   });
   $("#donald").on("mouseout", () => {
-    $("#donald").attr("src", "img/Goofi_2.webp");
+    $("#donald").attr("src", Goofi_2);
   });
 });
