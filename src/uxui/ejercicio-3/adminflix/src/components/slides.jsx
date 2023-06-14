0aa99cecfc2f588449a19346a30ece8f334b6ee8
SalvaToro <sftpo@protonmail.ch>
2023-06-14 12:42:01 +0200

    chore: UXUI ex3 changed to dracula theme for reveal.js

--- src/uxui/ejercicio-3/adminflix/src/components/slides.jsx
+++ src/uxui/ejercicio-3/adminflix/src/components/slides.jsx
@@ -2,7 +2,7 @@ import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.js";
 import { useEffect } from "react";
 import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/theme/black.css";
+import "reveal.js/dist/theme/dracula.css";
 import Presentation from "./presentation.jsx";
 
 function Slides() {
